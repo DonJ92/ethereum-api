3e7aeee44834a6b75e57e8e883414d16fa6b507c
Don Jung <don.j_1116@outlook.com>
2021-12-22 17:19:33 +0900

    update show PLT Token balance

--- src/plt.ts
+++ src/plt.ts
@@ -1,13 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 import { convertHexToString, convertStringToNumber } from "./bignumber";
 
-const apiUrl = "https://stag.palettescan.com/v1/";
+const apiUrl = "https://b.hashsuite.link:22000/v1/";
 
 export const pltGetAccountBalance =async (address: string): Promise<number> => {
     const url = `${apiUrl}/pltholderinfo`;
-console.log(address);
+
     const response = await axios.post(url, {
-        Address: address
+        Address: "0x23ad49edb638bd7402d398d2d1bc202ea811577d"
 //        Address: "0x2c4aad90f6e753ee68149a2e49987e44fb2e6f92"
     });
 
