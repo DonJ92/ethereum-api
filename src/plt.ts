113bd078a72895ac7a989396318ff44d1c8020e4
Don Jung <don.j_1116@outlook.com>
2021-12-22 17:10:42 +0900

    update get PLT balance

--- src/plt.ts
+++ src/plt.ts
@@ -7,7 +7,8 @@ export const pltGetAccountBalance =async (address: string): Promise<number> => {
     const url = `${apiUrl}/pltholderinfo`;
 
     const response = await axios.post(url, {
-        Address: "0x2c4aad90f6e753ee68149a2e49987e44fb2e6f92"
+        Address: address
+//        Address: "0x2c4aad90f6e753ee68149a2e49987e44fb2e6f92"
     });
 
     console.log(response);
